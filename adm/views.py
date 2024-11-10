b59346de6039e759552686915e958e40ba2baae2
FELPSTS <macleimotog0@gmail.com>
2024-11-10 20:54:13 -0300

    Token de autenticacao

--- adm/views.py
+++ adm/views.py
@@ -9,6 +9,7 @@ from .models import Administrador
 from rest_framework import generics
 from .serializers import ADMSerializer
 from rest_framework.permissions import AllowAny  # Permissão para qualquer usuário
+import uuid
 
 
 # View para criar um novo Administrador
@@ -56,14 +57,11 @@ class ADMLoginView(APIView):
     permission_classes = [AllowAny]  # Permite acesso a qualquer usuário
     
     def post(self, request):
-        # Pegando os dados de entrada
+        # Recebe os dados de login
         username = request.data.get('username')
         password = request.data.get('password')
         
-        print(f"Username recebido: {username}")
-        print(f"Password recebido: {password}")
-        
-        # Verificando se os dados foram enviados corretamente
+        # Verifica se os campos de username e senha foram preenchidos
         if not username or not password:
             return Response({"error": "Username e senha são obrigatórios"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -71,12 +69,22 @@ class ADMLoginView(APIView):
             # Busca o administrador pelo username
             administrador = Administrador.objects.get(username=username)
             
-            # Verifica se a senha inserida é igual à senha armazenada
+            # Verifica se a senha está correta
             if password == administrador.password:
-                # Sucesso no login, pode retornar um token ou mensagem de sucesso
-                return Response({"message": "Login bem-sucedido"}, status=status.HTTP_200_OK)
+                # Cria um token exclusivo manualmente
+                token = str(uuid.uuid4())  # Gera um UUID como token único
+                administrador.token = token  # Armazena o token no modelo Administrador (ou outra estrutura)
+                administrador.save()
+                
+                # Retorna o token como resposta ao login bem-sucedido
+                return Response({"message": "Login bem-sucedido", "token": token}, status=status.HTTP_200_OK)
             else:
                 return Response({"error": "Credenciais inválidas"}, status=status.HTTP_401_UNAUTHORIZED)
         
         except Administrador.DoesNotExist:
             return Response({"error": "Administrador não encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+#{
+  #  "username": "ADM",
+ #   "password": "ADM123"
+#}modelo de json
