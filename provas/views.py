79033af0889b39de24a21f471b32b5b950b1766c
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2024-11-19 15:38:02 -0300

    Update views.py

--- provas/views.py
+++ provas/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Prova
+from rest_framework import generics
+from .serializers import PROVASerializer
+from rest_framework.permissions import AllowAny  # Permissão para qualquer usuário
 
-# Create your views here.
+class PROVAreateView(generics.CreateAPIView):
+    queryset = Administrador.objects.all()
+    serializer_class = PROVASerializer
+    permission_classes = [AllowAny]
+
+class PROVAListView(generics.ListAPIView):
+    queryset = Administrador.objects.all()
+    serializer_class = PROVASerializer
+    permission_classes = [AllowAny]
+
+class PROVADetailView(generics.detailAPIview):
+    queryset = Administrador.objects.all()
+    serializer_class = PROVASerializer
+    permission_classes = [AllowAny]
+
+class PROVADetailView(generics.deleteAPIview):
+    queryset = Administrador.objects.all()
+    serializer_class = PROVASerializer
+  permission_classes = [AllowAny]
