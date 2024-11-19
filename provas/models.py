d65391d7915e1644ff2de82dbc3da1cc35818402
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2024-11-19 16:33:00 -0300

    Update models.py

--- provas/models.py
+++ provas/models.py
@@ -5,7 +5,6 @@ class PROVA(models.Model):
     nome = models.CharField(max_length=255)
     ID_Curso_pertencente = models.CharField(max_length=255)
     ID_funcionario = models.CharField(max_length=255)
-    pertencente = FileField(upload_to='provas/')
     arquivo_prova = models.FileField(upload_to='provas/')
     progress = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     
