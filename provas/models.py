f90f78e851ef00e1f50eed3bccc8e3eb327ee31a
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2024-11-19 15:51:26 -0300

    Update models.py

--- provas/models.py
+++ provas/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from djongo import models  # Se estiver utilizando djongo para integração com MongoDB
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+class PROVA(models.Model):
+
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Chama o método save() da classe pai para salvar o objeto
