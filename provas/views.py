a1f48539b802ae3dd113e679a621fdb3a7f1b046
Felipe Torquato <112289573+FELPSTS@users.noreply.github.com>
2024-11-21 15:22:21 -0300

    Add files via upload

--- provas/views.py
+++ provas/views.py
@@ -1,27 +1,32 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .models import Prova
+from .models import PROVA
 from rest_framework import generics
 from .serializers import PROVASerializer
 from rest_framework.permissions import AllowAny  # Permissão para qualquer usuário
 
-class PROVAreateView(generics.CreateAPIView):
-    queryset = Administrador.objects.all()
+class PROVACreateView(generics.CreateAPIView):
+    queryset = PROVA.objects.all()
     serializer_class = PROVASerializer
     permission_classes = [AllowAny]
 
 class PROVAListView(generics.ListAPIView):
-    queryset = Administrador.objects.all()
+    queryset = PROVA.objects.all()
     serializer_class = PROVASerializer
     permission_classes = [AllowAny]
 
 class PROVADetailView(generics.detailAPIview):
-    queryset = Administrador.objects.all()
+    queryset = PROVA.objects.all()
     serializer_class = PROVASerializer
     permission_classes = [AllowAny]
 
-class PROVADetailView(generics.deleteAPIview):
-    queryset = Administrador.objects.all()
+class PROVAUpdateView(generics.deleteAPIview):
+    queryset = PROVA.objects.all()
     serializer_class = PROVASerializer
-  permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
+
+class PROVADeleteView(generics.deleteAPIview):
+    queryset = PROVA.objects.all()
+    serializer_class = PROVASerializer
+    permission_classes = [AllowAny]
